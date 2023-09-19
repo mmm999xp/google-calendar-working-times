6ea57b973d200f9b0660694ae1165ce0d3248990
mmm999xp <mmm999xp@gmail.com>
2023-09-19 11:04:22 +0800

    feat: add total cost times function and show total cost times

--- JavaScript/form.js
+++ JavaScript/form.js
@@ -5,13 +5,13 @@ const peopleCountsInput = document.querySelector('#people_counts_input')
 const workinghoursInput = document.querySelector('#working_hours_input')
 const table = document.querySelector('.custom-table')
 const container = document.querySelector('.container')
-const date = document.querySelector('#date_input').value
+const date = document.querySelector('#date_input')
 const productNumber = document.querySelector('#product_number_input')
 const productColor = document.querySelector('#product_color_input')
 const workingHours = document.querySelector('#working_hours_input')
 const peopleCounts = document.querySelector('#people_counts_input')
 const productCounts = document.querySelector('#product_counts_input')
-const allCostTimes = document.querySelectorAll('.cost_times')
+const totalCostTimes = document.querySelector('#total_cost_times')
 /* 監聽器 */
 peopleCountsInput.addEventListener('input', showPeopleValue)
 workinghoursInput.addEventListener('input', showHoursValue)
@@ -21,7 +21,7 @@ container.addEventListener('input', costTimesInputChange)
 
 submitButton.addEventListener('click', () => {
   /* ======取得資料====== */
-  const dateValue = document.querySelector('#date_input').value
+  const dateValue = date.value
   const productNumberValue = productNumber.value.toUpperCase()
   const productColorValue = productColor.value
   const workingHoursValue = workingHours.value
@@ -57,6 +57,7 @@ submitButton.addEventListener('click', () => {
        `
 
         table.innerHTML += tableHTML
+        totalCostTimes.innerText = sumCostTimes(workingHoursValue)
       })
     })
     .catch(error => console.log(error))
@@ -64,9 +65,13 @@ submitButton.addEventListener('click', () => {
 // 給定平均工時、作業人數、每人每日做的時間、生產數量，回傳完成的時間(以X天X小時顯示)
 function costTimes (processTimes, peopleCounts, workingHours, productCounts) {
   let totalHours = productCounts / (peopleCounts * workingHours * processTimes) // 總小時
-  const day = Math.floor(totalHours)
+  let day = Math.floor(totalHours)
   totalHours -= day
-  const hour = Math.ceil(totalHours * workingHours)
+  let hour = Math.ceil(totalHours * workingHours)
+  if (hour >= workingHours) {
+    hour = hour - workingHours
+    day = day + 1
+  }
   return day + '天' + hour + '小時'
 }
 /* 顯示input的值 */
@@ -98,9 +103,11 @@ function addAndDeleteProcess (event) {
 
     table.innerHTML += tableHTML
   }
-  // 如果點擊到刪除按鈕，將table的一整行(table row)刪除
+  // 如果點擊到刪除按鈕，將table的一整行(table row)刪除，同時呼叫sumCostTimes更新數值
   if (target.classList.contains('delete_process')) {
+    const workingHoursValue = workingHours.value
     target.parentElement.parentElement.remove()
+    totalCostTimes.innerText = sumCostTimes(workingHoursValue)
   }
 }
 /* ============= */
@@ -108,7 +115,7 @@ function addAndDeleteProcess (event) {
 function costTimesInputChange (event) {
   let newProcessTimes
   const target = event.target
-
+  const allCostTimes = document.querySelectorAll('.cost_times') // 有可能透過API或是新增按鈕增加table欄位，因此必須重新抓取節點
   // console.log(target)
   if (target.classList.contains('process_times_input') || target === peopleCounts || target === workingHours || target === productCounts) {
     allCostTimes.forEach(item => {
@@ -118,9 +125,39 @@ function costTimesInputChange (event) {
       const workingHoursValue = workingHours.value || 0
       const peopleCountsValue = peopleCounts.value || 0
       const productCountsValue = productCounts.value || 0
+      // console.log('newProcessTimes', newProcessTimes)
+      // console.log('workingHoursValue', workingHoursValue)
+      // console.log('peopleCountsValue', peopleCountsValue)
+      // console.log('productCountsValue', productCountsValue)
       // 呼叫costTimes 重新計算工時
       const newCostTimes = costTimes(newProcessTimes, peopleCountsValue, workingHoursValue, productCountsValue)
       item.innerText = newCostTimes
+      totalCostTimes.innerText = sumCostTimes(workingHoursValue)
     })
   }
+}
+
+// 計算總耗時，每次此函式重新抓取CostTimes總節點，而不需要作為參數傳入，這是因為可能會因為透過API或是新增按鈕增加數個CostTime欄位
+// 需要傳入每日工作時間的參數，這是為了可以超過工作時間時進位，假設一天工作8小時，在計算出結果為14小時9小時會進位成15天1小時
+function sumCostTimes (workingHours) {
+  const allCostTimes = document.querySelectorAll('.cost_times')
+  let totalDays = 0
+  let totalHours = 0
+  allCostTimes.forEach(item => {
+    // 提取天數與小時數
+    const matches = item.innerText.match(/(\d+)天(\d+)小時/)
+
+    if (matches) {
+      const days = parseInt(matches[1]) // 提取天數
+      const hours = parseInt(matches[2]) // 提取小時數
+      totalDays = totalDays + days
+      totalHours = totalHours + hours
+      // 處理時間進位
+      if (totalHours > workingHours) {
+        totalHours = totalHours - workingHours
+        totalDays = totalDays + 1
+      }
+    }
+  })
+  return '總共花費: ' + totalDays + '天' + totalHours + '小時'
 }
